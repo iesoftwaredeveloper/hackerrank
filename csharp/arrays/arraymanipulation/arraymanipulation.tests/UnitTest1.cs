339e178a0f729d4ad348e5de380629d6c480cae3
Robert Little <rll04747@pomona.edu>
2021-07-04 14:43:21 -0700

    Initial solution

---
+++ csharp/arrays/arraymanipulation/arraymanipulation.tests/UnitTest1.cs
@@ -0,0 +1,60 @@
+using System;
+using Xunit;
+using System.Collections.Generic;
+
+namespace arraymanipulation.tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            int n = 10, m = 3;
+            List<List<int>> queries = new() {
+                new List<int>{1,5,3},
+                new List<int>{4,8,7},
+                new List<int>{6,9,1}
+            };
+            var sut = Result.arrayManipulation(n, queries);
+            Assert.Equal(10, sut);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            int n = 5, m = 3;
+            List<List<int>> queries = new() {
+                new List<int>{1,2,100},
+                new List<int>{3,5,100},
+                new List<int>{3,4,100}
+            };
+            var sut = Result.arrayManipulation(n, queries);
+            Assert.Equal(200, sut);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            int n = 10, m = 4;
+            List<List<int>> queries = new() {
+                new List<int>{2,6,8},
+                new List<int>{3,5,7},
+                new List<int>{1,8,1},
+                new List<int>{5,8,15}
+            };
+            var sut = Result.arrayManipulation(n, queries);
+            Assert.Equal(31, sut);
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            int n = 10, m = 0;
+            List<List<int>> queries = new() {
+            };
+            var sut = Result.arrayManipulation(n, queries);
+            Assert.Equal(0, sut);
+        }
+
+    }
+}
---
+++ csharp/arrays/arraymanipulation/arraymanipulation/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Numerics;
+
+namespace arraymanipulation
+{
+    /// <summary>
+    /// Starting with a 1-indexed array of zeros and a list of operations, 
+    /// for each operation add a value to each the array element between two 
+    /// given indices, inclusive. Once all operations have been performed, 
+    /// return the maximum value in the array. 
+    /// </summary>
+    public class Result
+    {
+
+        /*
+         * Complete the 'arrayManipulation' function below.
+         *
+         * The function is expected to return a LONG_INTEGER.
+         * The function accepts following parameters:
+         *  1. INTEGER n
+         *  2. 2D_INTEGER_ARRAY queries
+         */
+
+        public static long arrayManipulation(int n, List<List<int>> queries)
+        {
+            // Initialize array
+            long[] arr = new long[n];
+            long max = 0;
+
+            // Iterate over each set of instructions.
+            foreach(var abk in queries)
+            {
+                
+                // Start at index a, iterate till index b
+                // Add k to the value at the index.
+                // Is there a faster way to do this than a for loop?
+                for(int i = abk[0]-1; i <=abk[1]-1 && i < n; i++)
+                {
+                    arr[i] += abk[2];
+                }
+            }
+            return arr.Max();
+        }
+
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+            int n = Convert.ToInt32(firstMultipleInput[0]);
+
+            int m = Convert.ToInt32(firstMultipleInput[1]);
+
+            List<List<int>> queries = new List<List<int>>();
+
+            for (int i = 0; i < m; i++)
+            {
+                queries.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(queriesTemp => Convert.ToInt32(queriesTemp)).ToList());
+            }
+
+            long result = Result.arrayManipulation(n, queries);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+}
