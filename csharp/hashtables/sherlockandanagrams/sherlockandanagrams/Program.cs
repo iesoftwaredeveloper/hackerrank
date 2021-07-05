874427b0720ba8c22222345fcfa4c0b5470f6d67
Robert Little <rll04747@pomona.edu>
2021-07-05 08:17:02 -0700

    Reduce loops

--- csharp/hashtables/sherlockandanagrams/sherlockandanagrams/Program.cs
+++ csharp/hashtables/sherlockandanagrams/sherlockandanagrams/Program.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Linq;
 using System.IO;
-
+using System.Collections;
+using System.Collections.Generic;
 namespace sherlockandanagrams
 {
     public class Result
@@ -19,8 +20,9 @@ namespace sherlockandanagrams
             // The brute force method
             // Compare each possible substring and see 
             // if it can be found in the existing string.
-            // The complexity of this approach is too time consuming.
-            // O((n-1)^2*(n-1)
+            //
+            // Still time consuming, but acceptable if size is small enough.
+            // Improves by reducing the number of loops.
 
             // Need to check for two things
             // Are the letters matching?
@@ -34,30 +36,47 @@ namespace sherlockandanagrams
             // Initialize as zero matches
             int pairCnt = 0;
             int slen = s.Length;
+            Dictionary<string, int> hash = new Dictionary<string, int>();
             // Check for anagrams of length 1 to n-1
             for (int n = 1; n < slen; n++)
             {
+
                 // Start at first index length of n
-                for (int i = 0; i < slen; i++)
+                for (int i = 0; i+n <= slen; i++)
                 {
-                    // Look for n length matches
-                    // Must not include all chars of i
-                    for (int j = i + 1; j + (n - 1) < slen; j++)
+                    // Get the current substring and sort.
+                    var s1 = String.Concat(s.Substring(i, n).OrderBy(a => a));
+
+                    // If we have seen this before, increment count.
+                    // Otherwise, add it to hash as it is the first instance.
+                    if(hash.TryGetValue(s1, out int count))
                     {
-                        var s1 = String.Concat(s.Substring(i, n).OrderBy(a => a));
-                        var s2 = String.Concat(s.Substring(j, n).OrderBy(a => a));
-                        // If two sorted strings match, then is anagram
-                        if(s1.Equals(s2))
-                        {
-                            pairCnt += 1;
-                        }
+                        pairCnt += count;
+                        hash[s1]++;
+                        continue;
                     }
+                    hash.Add(s1, 1);
                 }
             }
 
             return pairCnt;
         }
 
+/// <summary>
+/// Determine if s1 and s2 are anagrams
+/// </summary>
+/// <param name="s1"></param>
+/// <param name="s2"></param>
+/// <returns></returns>
+        static bool isAnagram(string s1, string s2)
+        {
+            if(s1.Length != s2.Length)
+                return false;
+            
+            if(String.Concat(s1.OrderBy(a => a)).Equals(String.Concat(s2.OrderBy(a => a))))
+                return true;
+            return false;
+        }
     }
     class Program
     {
