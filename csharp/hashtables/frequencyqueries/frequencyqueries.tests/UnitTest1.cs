2f27421e20beaa7cbaecde879c55fd174de99ed8
Robert Little <rll04747@pomona.edu>
2021-07-05 15:34:18 -0700

    Initial implementation

---
+++ csharp/hashtables/frequencyqueries/frequencyqueries.tests/UnitTest1.cs
@@ -0,0 +1,82 @@
+using System;
+using Xunit;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace frequencyqueries.tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            List<List<int>> queries = new List<List<int>>()
+            {
+                new(){ 1, 1 },
+                new(){ 2, 2 },
+                new(){ 3, 2 },
+                new(){ 1, 1 },
+                new(){ 1, 1 },
+                new(){ 2, 1 },
+                new(){ 3, 2 }
+            };
+            List<int> expected = new List<int>() { 0, 1 };
+            var sut = Program.freqQuery(queries);
+            Assert.Equal(expected, sut);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            List<List<int>> queries = new List<List<int>>()
+            {
+                new(){ 1, 5 },
+                new(){ 2, 6 },
+                new(){ 3, 2 },
+                new(){ 1, 10 },
+                new(){ 1, 10 },
+                new(){ 1, 6 },
+                new(){ 2, 5 },
+                new(){ 3, 2 }
+            };
+            List<int> expected = new List<int>() { 0, 1 };
+            var sut = Program.freqQuery(queries);
+            Assert.Equal(expected, sut);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            List<List<int>> queries = new List<List<int>>()
+            {
+                new(){ 3,4 },
+                new(){ 2, 1003 },
+                new(){ 1, 16 },
+                new(){ 3,1 }
+            };
+            List<int> expected = new List<int>() { 0, 1 };
+            var sut = Program.freqQuery(queries);
+            Assert.Equal(expected, sut);
+        }
+        [Fact]
+        public void Test4()
+        {
+            List<List<int>> queries = new List<List<int>>()
+            {
+                new(){ 1, 3 },
+                new(){ 2, 3 },
+                new(){ 3, 2 },
+                new(){ 1, 4 },
+                new(){ 1, 5 },
+                new(){ 1, 5 },
+                new(){ 1, 4 },
+                new(){ 3, 2 },
+                new(){ 2, 4 },
+                new(){ 3, 2 }
+            };
+            List<int> expected = new List<int>() { 0, 1, 1 };
+            var sut = Program.freqQuery(queries);
+            Assert.Equal(expected, sut);
+        }
+    }
+}
---
+++ csharp/hashtables/frequencyqueries/frequencyqueries/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace frequencyqueries
+{
+    public class Program
+    {
+        // Complete the freqQuery function below.
+        public static List<int> freqQuery(List<List<int>> queries)
+        {
+            // Declare an empty list.
+            List<int> frequencies = new List<int>();
+            Dictionary<int, int> data = new Dictionary<int, int>();
+
+            // Given a list of operations, perform them.
+            // We have to iterate through each operation.
+            foreach (List<int> xy in queries)
+            {
+                int op = xy[0];
+                int val = xy[1];
+                switch (op)
+                {
+                    case 1: // Insert val
+                        if (data.Keys.Contains(val))
+                        {
+                            data[val] += 1; // Increment frequency
+                        }
+                        else
+                        {
+                            data.Add(val, 1); // Insert and set frequency
+                        }
+                        break;
+                    case 2: // Delete occurrence
+                        if (data.Keys.Contains(val))
+                        {
+                            data[val] -= 1;
+                            if (data[val] < 0)
+                                data[val] = 0;
+                        }
+                        break;
+                    case 3: // Check Frequency
+                        // Select any values == 2.
+                        if (data.Values.Any(x => x==val))
+                        {
+                            frequencies.Add(1);
+                        }
+                        else
+                        {
+                            frequencies.Add(0);
+                        }
+                        break;
+                }
+            }
+            return frequencies;
+        }
+
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int q = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<List<int>> queries = new List<List<int>>();
+
+            for (int i = 0; i < q; i++)
+            {
+                queries.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(queriesTemp => Convert.ToInt32(queriesTemp)).ToList());
+            }
+
+            List<int> ans = freqQuery(queries);
+
+            textWriter.WriteLine(String.Join("\n", ans));
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+}
